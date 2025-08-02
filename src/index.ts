0db7a1cd16abe65b4c0edcb0ee7bdb40e49b8311
Srigi <295197+srigi@users.noreply.github.com>
2025-08-02 14:12:00 +0200

    Refactor logging with --debug flag
    --debug allows to specify the absolute path of a log-file now

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
-import { resolve } from 'node:path';
-import winston, { transports } from 'winston';
 
 import { version } from '../package.json';
+import { createLogger } from '~/logger';
+import { parseDebugArgs } from '~/utils/cli/parseDebugArgs';
+import { tryCatch } from '~/utils/tryCatch';
 import { schema as searchImageSchema, handler as searchImageHandler } from '~/tools/search_image';
 import { schema as persistImageSchema, handler as persistImageHandler } from '~/tools/persist_image';
-import { createLogger } from '~/logger';
 
-const logger = createLogger({
-  level: process.argv.includes('--debug') ? 'debug' : 'info',
-  format: winston.format.json({ ...(process.argv.includes('--pretty-print') && { space: 2 }) }),
-  defaultMeta: { service: 'mcp-google-images-search' },
-  transports: [new transports.File({ filename: resolve(__dirname, '..', 'logs', 'info.log') })],
-});
+const [parseErr, debugConfig] = tryCatch<Error, ReturnType<typeof parseDebugArgs>>(parseDebugArgs);
+if (parseErr != null) {
+  console.error('Error parsing the --debug argument(s):', parseErr.message);
+  process.exit(1);
+}
+
+const logger = createLogger({ debugConfig, prettyPrint: process.argv.includes('--pretty-print') });
 const server = new McpServer({ name: 'Google images search', version });
 
 server.tool('search_image', 'Search the image(s) online', searchImageSchema, searchImageHandler);
